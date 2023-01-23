594bac9744295452890d26ccbc89f602e9dce95e
Holly <hollycmanganoni@gmail.com>
2023-01-23 20:24:54 +0000

    Second commit

--- client/src/Recipe.js
+++ client/src/Recipe.js
@@ -1,4 +1,21 @@
 import App from "./App";
-import "./App.css";
+import style from "./App.css";
+
+
+const Recipe = ({title, image, ingredients}) => {
+    return (
+        <div className={style.recipe}>
+            <h1>{title}</h1>
+            <ol>
+                {ingredients.map (ingredient => (
+                    <li key={ingredient.id}>{ingredient.text}</li>
+                ))}
+                </ol>
+                
+
+            <img className={style.image} src={image} alt="" />
+        </div>
+    )
+}
 
 export default Recipe;
