697e6baa427ed0b1cb2f1b622087945757eceb9d
Holly <hollycmanganoni@gmail.com>
2023-01-23 20:25:27 +0000

    Second commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import Recipe from "./Recipe";
 import Form from "./Form";
 import './App.css';
 
-function App() {
+const App = () => {
   //API information
   const APP_ID = '5de39993';
   const APP_KEY = 'e9ea68aa381f308697b567385ff3868e';
 
 //Use states
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
   const [query, setQuery] = useState("");
 
   useEffect(() => {
@@ -24,6 +25,17 @@ function App() {
     setRecipes(data.hits);
     console.log(data);
   };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("")
+  }
+
   
     
     
@@ -31,7 +43,29 @@ function App() {
   return (
     <div className="App">
       <h1>Eat ASAP app</h1>
-      <Form></Form>
+      
+      <form className="search-box" onSubmit={getSearch}>
+        <input className="search-bar" 
+        type="text" 
+        placeholder="Chicken, peppers etc" 
+        onChange={updateSearch} />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+
+      </form>
+
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
+          instructions={recipe.recipe.instructions}
+          />
+        ))}
+      </div>
       
     </div>
   );
