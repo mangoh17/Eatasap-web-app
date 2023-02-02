477a566c023fc047e0e40cf6e5c435fb3e7078fc
Holly <hollycmanganoni@gmail.com>
2023-02-02 17:19:34 +0000

    Fifth save

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-const db = require(".../model/helper")
+const db = require("../model/helper")
 var app = express();
 
 app.use(logger('dev'));
